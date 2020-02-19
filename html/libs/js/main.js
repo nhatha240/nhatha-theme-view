e62718af4469784992fc28fa0289b7bf871bf081
nhatha240 <nhathafanskt@gmail.com>
2020-02-19 18:16:31 +0700

    end day 1

--- html/libs/js/main.js
+++ html/libs/js/main.js
@@ -1,17 +1,2 @@
 $(document).ready(function() {
-    $(window).resize(function() {
-        var x = $(window).width();
-        console.log(x);
-    });
-    var mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: true
-    });
-    $('.btn-sch').on('click', function() {
-        $(this).parent('.nav-item').find('.search').css {
-            "display",
-            "block"
-        }
-    });
 });
