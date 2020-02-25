86c4aca6f506089b4bc0bd29b97ebeaf50dab64d
nhatha240 <nhathafanskt@gmail.com>
2020-02-25 15:37:26 +0700

    day 2

--- html/libs/js/main.js
+++ html/libs/js/main.js
@@ -1,2 +1,9 @@
 $(document).ready(function() {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        // autoplay: {
+        //     delay: 1000,
+        //     disableOnInteraction: false,
+        // },
+    });
 });
